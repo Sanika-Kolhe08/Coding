55757bc7e812b12fe8369cc35f00221d287c4704
Sanika Kolhe <sanikakolhe127@gmail.com>
2026-07-10 13:54:22 +0530

    Add files via upload

---
+++ program110.c
@@ -0,0 +1,28 @@
+//Input : 8
+//Output : 2  4  6  8  10  12  14  16   
+
+#include <stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <=iNo;iCnt++)
+    {
+        printf("%d\t",iCnt*2);
+    }
+
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter a number : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+
+    return 0;
+}
