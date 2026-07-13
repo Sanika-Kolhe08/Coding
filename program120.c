0fed1c73afbcd6a6e3942621eaddbbb0633b79f9
Sanika Kolhe <sanikakolhe127@gmail.com>
2026-07-13 11:43:25 +0530

    Add files via upload

---
+++ program120.c
@@ -0,0 +1,40 @@
+//Input iRow = 3 iCol = 4
+/*
+    1   2   3   4
+    5   6   7   8
+    9   10  11  12
+*/
+
+#include <stdio.h>
+
+void Display(int iNo1, int iNo2)
+{
+    int i = 0,j = 0;
+    int iNum = 1;
+
+    for(i = iNo1; i>0; i--)
+    {
+        for(j = 1;j<= iNo2; j++)
+        {
+           printf("%d\t",iNum);
+           iNum++;
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 =0;
+
+    printf("Enter rows : ");
+    scanf("%d",&iValue1);
+
+    printf("Enter cols : ");
+    scanf("%d",&iValue2);
+
+    Display(iValue1,iValue2);
+
+    return 0;
+}
