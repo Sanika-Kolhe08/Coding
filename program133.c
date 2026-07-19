e43e5323f362ae8c4d5a845bae6ccbf564e0696c
Sanika Kolhe <sanikakolhe127@gmail.com>
2026-07-19 09:37:03 +0530

    Add files via upload

---
+++ program133.c
@@ -0,0 +1,54 @@
+//Accept number ofrows and number of columns from user and display below pattern
+
+//Input : iRow = 4 , iCol =4
+
+//Output : 
+
+/* 
+    $   *   *   *
+    #   $   *   *
+    #   #   $   *
+    #   #   #   $
+*/
+
+#include <stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j=0;
+    for(i = 0; i<iRow;i++)
+    {
+        for(j=0;j<iCol;j++)
+        {
+            if(j==i)
+            {
+                printf("$\t");
+            }
+            else if(i<j)
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf("#\t");
+            }
+            
+        }
+        printf("\n");
+    }
+
+}
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows: \n");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns: \n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
