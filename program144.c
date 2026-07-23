1bf8979da734c79d7ab8ee7d1ce1607f2064b76a
Sanika Kolhe <sanikakolhe127@gmail.com>
2026-07-23 18:28:51 +0530

    Add files via upload

---
+++ program144.c
@@ -0,0 +1,43 @@
+//Accept Character from user and check whether it is lower case alphabet or not
+
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL Check(char ch)
+{
+    if((ch >= 'a' && ch <= 'z' ))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+
+}
+
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("Enter a character: ");
+    scanf("%c",&cValue);
+
+    bRet = Check(cValue);
+
+    if(bRet == 1)
+    {
+        printf("It is a Small Case");
+    }
+    else
+    {
+        printf("It is not a Small Case");
+    }
+
+    return 0;
+}
